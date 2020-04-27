f5a38e6f6503020562aee32e4db8aee34c263aa0
Hércules <herculesdeso@gmail.com>
2020-04-27 11:22:25 -0300

    Starting to make login

--- project1/application.py
+++ project1/application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,4 +23,19 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+
+@app.route("/logged", methods=["POST"])
+def logged():
+    user_page = request.form.get("user")
+    password_page = request.form.get("password")
+    account_db = db.execute("SELECT * FROM accounts WHERE username=:username AND password=:password", {"username": user_page, "password": password_page}).fetchone()
+
+    if account_db is None:
+        return render_template("loggin_fail.html")
+
+    if account_db.username == user_page and account_db.password == password_page:
+        return render_template("hello.html", message="success")
+    else:
+        return render_template("loggin_fail.html")
