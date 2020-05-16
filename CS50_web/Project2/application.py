e1406e0891d066babdf2eafd0afcb2dc1aa5f354
Hércules de Sousa <herculesdeso@gmail.com>
2020-05-16 18:06:46 -0300

    Comunication between client and server working correctly

--- CS50_web/Project2/application.py
+++ CS50_web/Project2/application.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send
 
 messages = {'main': []}
 
@@ -20,11 +20,17 @@ def test():
     return render_template("chat.html")
 
 
+@socketio.on('confirmingConnection')
+def confirmingMessage(data):
+    text = data['data']
+    print(f'\033[1;32;40m{text}')
+
+
 @socketio.on("message_test")
-def print_message(data):
-    print(data['data'])
+def print_mes(data):
+    emit('receivingMessage', {'msg': data['data']}, broadcast=True)
 
 
-@socketio.on("message")
-def message(data):
-    print(data)
+@socketio.on('newUser')
+def newUser(data):
+    emit('receivingUser', {'user': data['data']}, broadcast=True)
